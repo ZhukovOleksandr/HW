df5f76b6f2a10d9c1083d97b593195cd1462a808
Zhukov Oleksandr <zhukov9523@gmail.com>
2021-02-17 16:26:58 +0200

    HW

--- HW6/lesson6_pract2.py
+++ HW6/lesson6_pract2.py
@@ -13,11 +13,40 @@ names = (
     'Luci Bannister', 'Eoin Gallegos', 'Rayhan Davie', 'Patrik Odling', 'Umayr Rice', 'Karim Hernandez', 'Tea Cope',
     'Etienne Gallagher', 'Sanjeev Rhodes', 'Shelbie Black', 'Juan Stokes', 'Dominykas Richardson', 'Tabitha Bautista')
 
-python = []
+python = set()
+frontend = set()
+fullstack = set()
+java = set()
 
-for i in names:
-    if len(python) < 5:
-        python.append(i)
-    else:
-        break
-print(python)
+while len(java) < 6:
+    python.add(choice(names))
+    frontend.add(choice(names))
+    fullstack.add(choice(names))
+    java.add(choice(names))
+
+
+
+student_dict = {'python': list(python), 'frontend': list(frontend),
+                'fullstack': list(fullstack), 'java': list(java)}
+
+several_group = python & frontend & fullstack & java
+not_front = (python | java | fullstack) - frontend
+py_or_java = python ^ java
+
+if len(several_group) == 0:
+    print('Никто из студентов не учиться в двух и более группах')
+else:
+    print('Вот список студентов, которые обучаются в двух и более группах:\n'
+          + str(several_group))
+
+if len(not_front) == 0:
+    print('На данный все студенты учаться на Frontend.')
+else:
+    print('Вот список студентов, которые не учатся на Frontend:\n'
+          + str(not_front))
+
+if len(py_or_java) == 0:
+    print('На данный момент все студенты обучаются в обеих группах.')
+else:
+    print('Вот список студентов, которые изучают Python или Java:\n'
+          + str(py_or_java))
